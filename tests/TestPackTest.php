6dfdf6e20daab0473212c4f461e5c45943013f9b
Flavio Leite <flaviorcleite@gmail.com>
2017-08-10 21:53:36 +0200

    Un intento más, a ver si ahora está todo

---
+++ tests/TestPackTest.php
@@ -0,0 +1,13 @@
+<?php
+use Brespit\TestPack\TestPack;
+
+class TestPackTest extends PHPUnit_Framework_TestCase
+{
+
+    public function test_package_uppercase()
+    {
+        $txt = "Flavio";
+        $test = new TestPack($txt);
+        $this->assertSame('FLAVIO', $test->upperCase());
+    }
+}
