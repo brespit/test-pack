510972a3846076d9729ae5cf12d1b32a94ddfd61
Flavio Leite <flaviorcleite@gmail.com>
2017-08-10 21:43:25 +0200

    First commit

---
+++ src/TestPack.php
@@ -0,0 +1,19 @@
+<?php
+namespace Brespit\TestPack;
+
+class TestPack
+{
+
+    protected $txt;
+
+    public function __construct($txt)
+    {
+        $this->txt = $txt;
+    }
+
+    public function upperCase()
+    {
+        return strtoupper($this->txt);
+    }
+
+}
